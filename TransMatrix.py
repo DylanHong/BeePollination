d8c8c094ad42caffcdef430271beac9ddefef617
DylanHong <Dylan.P.Hong.19@dartmouth.edu>
2019-05-01 14:08:37 -0400

    Updates to transition matrix

--- TransMatrix.py
+++ TransMatrix.py
@@ -1,6 +1,11 @@
 import numpy as np
 
 def transmat1(dim1,dim2):
-    matrix = np.zeros([dim1, dim2])
+    dim = dim1 * dim2
+
+    #Build matrix with zeros
+    matrix = np.zeros([dim, dim])
+
+    #populate transition matrix
 
     return matrix
