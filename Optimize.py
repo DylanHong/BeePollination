0245b0e18a1d35502c543be13f207bcfaa2e2b09
DylanHong <Dylan.P.Hong.19@dartmouth.edu>
2019-05-03 13:47:36 -0400

    Begin new transition matrix and start diffusion

--- Optimize.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-    def optimize(self,steps,days):
-        totals = []
-        for i in range(self.dims**2):
-            self.
--- TransMatrix.py
+++ TransMatrix.py
@@ -19,6 +19,7 @@ def transmat_simple(dims):
 
     return np.transpose(matrix)
 
+#Equal probability for all 10 squares adjacent
 def transmat_moderate(dims):
     dim_trans = dims**2  # transition matrix is n squared by n squared
 
@@ -29,15 +30,20 @@ def transmat_moderate(dims):
     for i in range(dim_trans):
         for j in range(dim_trans):
             if i == j:
-                matrix[i][j] += 0.2
-                matrix[i][min(dim_trans-1, j+1)] += 0.2
-                matrix[i][max(0, j-1)] += 0.2
-                matrix[i][min(j+dims, dim_trans-1)] += 0.2
-                matrix[i][max(0, j-dims)] += 0.2
+                matrix[i][j] += 0.1
+                matrix[i][min(dim_trans-1, j+1)] += 0.1
+                matrix[i][max(0, j-1)] += 0.1
+                matrix[i][min(j+dims, dim_trans-1)] += 0.1
+                matrix[i][max(0, j-dims)] += 0.1
+
+                matrix[i+1][min(dim_trans-1, j+1)] += 0.1
+                matrix[i+1][max(0, j-1)] += 0.1
+                matrix[i-1][min(j+dims, dim_trans-1)] += 0.1
+                matrix[i-1][max(0, j-dims)] += 0.1
 
     return np.transpose(matrix)
 
-
+#Normally distributed probabilities around central square two wide
 def transmat_advanced1(dims):
     dim_trans = dims**2  # transition matrix is n squared by n squared
 
@@ -56,7 +62,7 @@ def transmat_advanced1(dims):
 
     return np.transpose(matrix)
 
-
+#Implement boundaries with farm?
 def transmat_advanced2(dims):
     dim_trans = dims**2  # transition matrix is n squared by n squared
 
